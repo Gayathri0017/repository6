81545a1231dd559fc716858f18464f8d19766fd3
Gayathri0017 <gayathri4866@gmail.com>
2025-02-07 10:40:43 +0530

    Add.java

--- Add.java
+++ Add.java
@@ -1,32 +1,35 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+
 public class Add{
-	public static void main(String args[]) {
-		try {
-			File file=new File("input.txt");
-			Scanner sc=new Scanner(System.in);
-			int n=sc.nextInt();
-			int arr[]=new int[n];
-			for(int i=0;i<n;i++) {
-				arr[i]=sc.nextInt();
-			}
-			int odd=0;
-			int even=0;
-			for(int i=0;i<n;i++) {
-				if(arr[i]%2==0) {
-					even++;
-				}
-				else {
-					odd++;
-				}
-			}
-			System.out.println("Odd: "+odd);
-			System.out.println("Even: "+even);
-	}
-	catch(Exception e){
-		System.out.println("File not found");
-		e.printStackTrace();
-	}
-	}
+    public static void main(String[] args) {
+        try {
+            File file = new File("input.txt");
+            Scanner sc = new Scanner(file);
+            int size = sc.nextInt();
+            int arr[] = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int odd = 0, even = 0;
+
+            for (int i = 0; i < size; i++) {
+                if (arr[i] % 2 == 0) {
+                    even++;
+                } else {
+                    odd++;
+                }
+            }
+            System.out.println("Odd count: " + odd);
+            System.out.println("Even count: " + even);
+            sc.close();
+        } catch (IOException e) {
+            System.out.println("Error: File not found.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
