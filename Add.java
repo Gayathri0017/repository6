aedbefd60f73b188cfb58d747a641a610c64a06e
Gayathri0017 <gayathri4866@gmail.com>
2025-02-07 10:27:38 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Add{
+	public static void main(String args[]) {
+		try {
+			File file=new File("input.txt");
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			int arr[]=new int[n];
+			for(int i=0;i<n;i++) {
+				arr[i]=sc.nextInt();
+			}
+			int odd=0;
+			int even=0;
+			for(int i=0;i<n;i++) {
+				if(arr[i]%2==0) {
+					even++;
+				}
+				else {
+					odd++;
+				}
+			}
+			System.out.println("Odd: "+odd);
+			System.out.println("Even: "+even);
+	}
+	catch(Exception e){
+		System.out.println("File not found");
+	}
+	}
+}
