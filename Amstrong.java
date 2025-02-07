c6316c9ecc1de1d8654b005455bb77a1f0f73cdc
Gayathri0017 <gayathri4866@gmail.com>
2025-02-07 11:02:11 +0530

    Amstrong.java

--- Amstrong.java
+++ Amstrong.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.File;
 public class Amstrong{
 	    public static void main(String[] args) {
+	    try{
 	    File file=new File("input.txt");
 	    Scanner sc=new Scanner(System.in);
 	    int number,originalNumber, remainder, result = 0;
@@ -18,5 +19,8 @@ public class Amstrong{
 	            System.out.println(number + " is an Armstrong number.");
 	        else
 	            System.out.println(number + " is not an Armstrong number.");
+	    }catch(Exception e){
+		Systenm.out.println("error");
+	    }
 	    }
 	}
